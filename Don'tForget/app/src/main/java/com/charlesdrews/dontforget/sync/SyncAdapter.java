846966093d0b2c74f3903110440fbc21063015a1
charlesdrews <charlesdrews@gmail.com>
2016-03-29 17:36:11 -0400

    Add logic for using geolocation preference

--- Don'tForget/app/src/main/java/com/charlesdrews/dontforget/sync/SyncAdapter.java
+++ Don'tForget/app/src/main/java/com/charlesdrews/dontforget/sync/SyncAdapter.java
@@ -5,10 +5,13 @@ import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.SyncResult;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.charlesdrews.dontforget.R;
 import com.charlesdrews.dontforget.weather.WeatherHelper;
 import com.charlesdrews.dontforget.weather.model.HourlyForecast;
 
@@ -38,8 +41,26 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         Log.d(TAG, "onPerformSync: making API call");
 
-        //TODO - get geolocation
-        List<HourlyForecast> newHourlyForecasts = WeatherHelper.getHourlyForecasts("40.743043,-73.981797");
+        String query = null; //"40.743043,-73.981797"
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        // check if geolocation turned on in preferences
+        if (prefs.getBoolean(getContext().getString(R.string.pref_key_weather_geo), false)) {
+            Log.d(TAG, "onPerformSync: geolocation on");
+            //TODO - get last known location, save in query
+        } else {
+            // if not, then check if a static location has been set
+            Log.d(TAG, "onPerformSync: getting static location");
+            query = prefs.getString(getContext().getString(R.string.pref_key_weather_static_location), null);
+        }
+
+        if (query == null || query.equals(getContext().getString(R.string.weather_static_location_default))) {
+            Log.d(TAG, "onPerformSync: geolocation off & static location not set");
+            //TODO - ask user to do one or the other
+            return;
+        }
+
+        List<HourlyForecast> newHourlyForecasts = WeatherHelper.getHourlyForecasts(query);
 
         if (newHourlyForecasts != null && newHourlyForecasts.size() > 0) {
             Log.d(TAG, "onPerformSync: API call yielded results");
